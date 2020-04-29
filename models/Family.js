af28679e0201e4b27f2062522f36270f85846c50
Chantel Taki <chanteltaki@gmail.com>
2020-04-29 05:09:15 -0400

    updated models and routes to connect user to family

--- models/Family.js
+++ models/Family.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const familySchema = new Schema({
-  username:String,
-  password:String,
+username:String,
+password:String,
 familyName: String,
-members:{
-  type: [Schema.Types.ObjectId],
+members: [
+  {
+  type: Schema.Types.ObjectId,
   ref: 'User'
-},
+}
+],
 tasks:{
 type:Schema.Types.ObjectId,
 ref:'Task'
--- routes/private.js
+++ routes/private.js
@@ -3,11 +3,12 @@ const router  = express.Router();
 const User = require('../models/User');
 const Family = require('../models/Family');
 
-
 router.get('/private', (req, res, next) => {
-  const user=req.user;
-  console.log(req.user);
-  res.render('private', {user:user});
+  const familyID=req.user._id;
+  Family.findById(familyID).populate("members").then(family => {
+    console.log(family.members);
+    res.render('private');
+  })
+ 
 });
-
 module.exports = router;
