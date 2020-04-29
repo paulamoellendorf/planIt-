024a81f2a0cfcb156419d8d41149eb524ca15cb8
Chantel Taki <chanteltaki@gmail.com>
2020-04-29 05:02:35 -0400

    model changes

--- models/Family.js
+++ models/Family.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const familySchema = new Schema({
-  username:String,
-  password:String,
+username:String,
+password:String,
 familyName: String,
 members: [
   {
